d2183343cb278efc5970f23c90b002fdf4c6a502
Adrián Pérez <a.przlopez@gmail.com>
2024-05-12 17:31:31 +0200

    feat: create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { W3MLoginComponent } from './components/w3m-login/w3m-login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HasNetworkIdGuard } from './core/guards/has-netwrok-guard.service';
+import { DashboardComponent } from '@components/dashboard/dashboard.component';
+import { NotSubscribedComponent } from '@components/no-subscriber/not-subscribed.component';
+import { W3MLoginComponent } from '@components/w3m-login/w3m-login.component';
+import { HasNetworkIdGuard } from '@guards/has-netwrok-guard.service';
+import { IsSubscribedGuard } from '@guards/is-subscribed-guard.service';
 
 const routes: Routes = [
   { path: '', component: W3MLoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [HasNetworkIdGuard] },
+  { path: 'not-subscribed', component: NotSubscribedComponent }, // Fix when user enter directly here
+  { path: 'dashboard', component: DashboardComponent, canActivate: [HasNetworkIdGuard, IsSubscribedGuard] },
 ];
 
 @NgModule({
