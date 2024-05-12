b60ab45cee7559fa34082c1f5a0574f38e6e477c
Adrián Pérez <a.przlopez@gmail.com>
2024-05-12 13:19:23 +0200

    feat: add messages

---
+++ src/app/utils/messages.ts
@@ -0,0 +1,6 @@
+export const MESSAGES = {
+  NOT_SUBSCRIBED: {
+    TITLE: 'You´re not subscribed',
+    DETAIL: 'Want to stay one step ahead of the weather? Join our community today! Subscribe to our website to access accurate and up-to-date weather data for your area and around the world. Whether you´re a weather enthusiast, a professional, or just want to be prepared for any eventuality, our platform will provide you with the information you need. Dont miss out on the opportunity to stay informed and ready for any weather! Subscribe now and unlock a world of weather knowledge.',
+  }
+}
