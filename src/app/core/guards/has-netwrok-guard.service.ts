959791d034c9597abdfc118a1247a6e50d9d2e7f
Adrián Pérez <a.przlopez@gmail.com>
2024-05-12 13:24:24 +0200

    feat: create guard to check the network

--- src/app/core/guards/has-netwrok-guard.service.ts
+++ src/app/core/guards/has-netwrok-guard.service.ts
@@ -1,8 +1,8 @@
-import { CanActivateFn, Router } from "@angular/router";
-import { Web3ModalService } from "../services/web3modal.service";
+import { CanActivateFn } from "@angular/router";
 import { inject } from "@angular/core";
+import { Web3ModalService } from "@services/web3modal.service";
 
 export const HasNetworkIdGuard: CanActivateFn = () => {
   const web3ModalService = inject(Web3ModalService);
-  return (web3ModalService.web3Modal.getState().selectedNetworkId || 0) > 0 ? true : inject(Router).createUrlTree(['']);
+  return (web3ModalService.getState().selectedNetworkId || 0) > 0;
 };
