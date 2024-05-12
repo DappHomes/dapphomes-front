ee58b006a6f1a9e60246ff1bbf5b0c050d35ae3e
Adrián Pérez <a.przlopez@gmail.com>
2024-05-12 13:25:28 +0200

    feat: init taco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initialize } from '@nucypher/taco';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dapphomes-front';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    initialize().then();
+  }
 }
